8a80f639be9813e157373ac210845ee4d3ee7193
Justin Kruskie <kruskiej@baisd.net>
2024-02-24 16:38:49 +0000

    Create assignment

---
+++ FuelMixer.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class fuelMixer 
+{
+    public static void main(String[] args) 
+    {
+        // Named constant (hint: final) for the mixture.
+
+        // Variables for the adjusted mixture.
+
+        // Create a Scanner object for keyboard input.
+
+        // Get the amount of fuel being used.
+
+        // Close the keyboard scanner
+
+        // Calculate how much oxidizer is required for that much fuel.
+
+        // Calculate the total propellant weight
+
+        // Display the outputs.
+
+        // Close the program
+
+    }
+}
